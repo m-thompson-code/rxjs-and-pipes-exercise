aa307fe6e8a19ae340a19600a7b52b22abc21a54
Mark Thompson <mthompson@bitovi.com>
2022-04-26 09:12:06 -0700

    Added font

--- src/app/pipes/title.pipe.ts
+++ src/app/pipes/title.pipe.ts
@@ -7,10 +7,6 @@ export class TitlePipe implements PipeTransform {
   transform(value: string): string {
     // TODO: Update this code to uppercase the first letter of each word in the title
     // "la la land" => "La La Land"
-    // return value.toUpperCase();
-    return value
-      .split(' ')
-      .map((part) => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
-      .join(' ');
+    return value.toUpperCase();
   }
 }
